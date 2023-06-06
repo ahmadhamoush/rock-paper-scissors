6ec3687bc95dd49136086cdd9ac7676ddbddd66f
Ahmad Hamoush <hamoush@outlook.com>
2023-06-06 16:20:53 +0300

    added main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,39 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFistRaised, faHand, faHandBackFist, faHandFist, faHandScissors, faL } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import { toast , Toaster} from 'react-hot-toast'
 
 
 export default function Home() {
 
+  const choices = ['rock', 'paper', 'scissors'];
+  
+  const[player,setPlayer] = useState('')
+  const[computer,setComputer] = useState('')
+  const[result,setResult] = useState('')
+  const[isPlaying,setIsPlaying] = useState(false)
+  const[isFinshed,setIsFinished] = useState(false)
 
+  const shoot = ()=>{
+
+    if(player ===''){
+      toast.error('Please pick a choice')
+    }
+    else{
+      setIsPlaying(true)
+      setComputer(choices[Math.floor(Math.random()*choices.length)])
+      
+      setTimeout(()=>{
+        setIsPlaying(false)
+        setIsFinished(true)
+        toast.success('meow')
+      },3000)
+  
+    }
+  }
   return (
     <>
       <Head>
@@ -13,7 +42,28 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-  
+      <main className={styles.main}>
+
+        <Toaster />
+        <h1>Rock Paper Scissors Shoot!</h1>
+    <div className={styles.hands}>
+    {!isFinshed && <FontAwesomeIcon className={`${styles.fist} ${isPlaying && styles.leftHandAnime}`} icon={faHandBackFist} />}
+    {isFinshed && player ==='rock' &&  <FontAwesomeIcon className={styles.fist} icon={faFistRaised} /> }
+    {isFinshed && player ==='scissors' &&  <FontAwesomeIcon style={{transform: 'rotate(0) scaleX(-1)'}} className={styles.fist} icon={faHandScissors} /> }
+    {isFinshed && player ==='paper' &&  <FontAwesomeIcon className={styles.fist} icon={faHand} /> } 
+
+    <FontAwesomeIcon className={styles.fist} icon={faHandBackFist} />
+    </div>
+    <div className={styles.wrapper}>
+    <div className={styles.icons}>
+    <FontAwesomeIcon onClick={()=>setPlayer('rock')} className={`${styles.icon} ${player === 'rock' && styles.active}`} icon={faFistRaised} />
+    <FontAwesomeIcon onClick={()=>setPlayer('paper')} className={`${styles.icon} ${player === 'paper' && styles.active}`}  icon={faHand} />
+    <FontAwesomeIcon style={{transform: 'rotate(90deg)'}} onClick={()=>setPlayer('scissors')} className={`${styles.icon} ${player === 'scissors' && styles.active}`}  icon={faHandScissors} />
+    </div>
+    <button onClick={shoot} >Shoot</button>
+    </div>
+
+      </main>
     </>
   )
 }
